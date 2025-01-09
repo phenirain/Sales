0b69333d65d597a66829ade646305524f9bab551
phenirain <bigrus1998@gmail.com>
2025-01-10 01:03:28 +0300

    added exception handler

---
+++ Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Sales.Exceptions;
+
+namespace Sales.Middlewares;
+
+public class ExceptionHandlerMiddleware
+{
+    
+    private readonly RequestDelegate _next;
+    
+    public ExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception e)
+        {
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception e)
+    {
+        context.Response.ContentType = "application/json";
+
+        context.Response.StatusCode = e switch
+        {
+            InvalidPriceException => StatusCodes.Status400BadRequest,
+            InvalidTokenException => StatusCodes.Status401Unauthorized,
+            NotFoundException => StatusCodes.Status404NotFound,
+            OutOfStockException => StatusCodes.Status400BadRequest,
+            RepositoryNotFound => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var response = new
+        {
+            message = e.Message,
+        };
+        
+        await context.Response.WriteAsJsonAsync(response);
+    }
+
+
+}
--- Program.cs
+++ Program.cs
@@ -28,6 +28,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseMiddleware<AuthMiddleware>();
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 app.UseHttpsRedirection();
 
 app.Run();
