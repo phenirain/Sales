9028623e9b8e31e3fa7665ece8d39fb7180550e6
phenirain <bigrus1998@gmail.com>
2025-01-09 22:20:22 +0300

    edit not found exception

--- Exceptions/NotFoundException.cs
+++ Exceptions/NotFoundException.cs
@@ -1,5 +1,8 @@
 ﻿namespace Sales.Exceptions;
 
-public class NotFoundException(string dbModel, long id): Exception($"{dbModel} with id: {id} not found")
+public class NotFoundException: Exception
 {
+    public NotFoundException(Type DbModel, long id) : base($"{DbModel} with id: {id} not found") {}
+    
+    public NotFoundException(string message): base(message){}
 }
