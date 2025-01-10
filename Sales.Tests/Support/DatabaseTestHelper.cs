595d9668eff1d67786b910036e2b3018b84a36aa
phenirain <bigrus1998@gmail.com>
2025-01-10 18:55:13 +0300

    feat: added database test helper

---
+++ Sales.Tests/Support/DatabaseTestHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Contexts;
+
+namespace Sales.Tests.Support;
+
+public static class DatabaseTestHelper
+{
+    public static Context CreateTestContext()
+    {
+        var connectionString = DotNetEnv.Env.GetString("DbConnectionString");
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new InvalidOperationException("DbConnectionString is not set");
+        }
+
+        var options = new DbContextOptionsBuilder<Context>()
+            .UseNpgsql(connectionString)
+            .Options;
+        var context = new Context(options);
+        
+        context.Database.Migrate();
+        
+        return context;
+    }
+
+    public static void ResetDatabase(Context context)
+    {
+        context.Database.EnsureDeleted();
+        context.Dispose();
+    }
+}
