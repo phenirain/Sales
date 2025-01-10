fc5a73a2757e707d8b4ac8c28bfd69012580e2cf
phenirain <bigrus1998@gmail.com>
2025-01-10 21:16:53 +0300

    test: added tests for buyer repository with Error at CreateMethod

---
+++ Sales.Tests/Integration/Repositories/BuyerRepositoryTests.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using Bogus;
+using Sales.Contexts;
+using Sales.Entities.DbModels;
+using Sales.Entities.DomainModels;
+using Sales.Entities.Mapping;
+using Sales.Repositories;
+using Sales.Repositories.Interfaces;
+using Sales.Tests.Support;
+
+namespace Sales.Tests.Integration.Repositories;
+
+
+public class BuyerRepositoryTests
+{
+    private static IUnitOfWork _unitOfWork;
+    
+    [OneTimeSetUp]
+    public static async Task GlobalSetUp()
+    {
+        _unitOfWork = await DatabaseTestHelper.CreateUnitOfWork();
+        await DatabaseTestHelper.CreateFakeBuyers();
+    }
+
+    [Test]
+    public async Task GetAllBuyers_WithLimitAndOffset()
+    {
+        var actual = await _unitOfWork.BuyerRepository.GetAll(7, 0);
+        Assert.That(7, Is.EqualTo(actual.Count()));
+    }
+
+    [Test]
+    public async Task GetAllBuyers_WithSalesIds()
+    {
+        await DatabaseTestHelper.CreateFakeSales(buyerId: new Faker().Random.Number(1, 20));
+        var actual = await _unitOfWork.BuyerRepository.GetAll(7, 0);
+        var nullCount = 0;
+        var notExpected = 7;
+        foreach (var buyer in actual)
+        {
+            if (buyer.SalesIds.Count == 0)
+                nullCount++;
+        }
+        Assert.That(nullCount, Is.LessThan(notExpected));
+    }
+
+    [Test]
+    public async Task GetBuyerById_WithSalesIds()
+    {
+        var id = new Faker().Random.Number(1, 20);
+        await DatabaseTestHelper.CreateFakeSales(buyerId: id);
+        var actual = await _unitOfWork.BuyerRepository.GetById(id);
+        Assert.IsNotNull(actual);
+        Assert.That(actual.Id, Is.EqualTo(id));
+        Assert.That(actual.SalesIds.Count, Is.GreaterThan(0));
+    }
+
+    [Test]
+    public async Task CreateBuyer_And_GetById()
+    {
+        int id = 21;
+        var buyer = new Faker<Buyer>()
+           .RuleFor(b => b.Name, f => f.Name.FullName())
+           .Generate();
+        var actualId = await _unitOfWork.BuyerRepository.Create(buyer);
+        await _unitOfWork.SaveChangesAsync();
+        Assert.That(actualId, Is.GreaterThan(0));
+        Assert.That(actualId, Is.EqualTo(id));
+    }
+
+    [Test]
+    public async Task UpdateBuyer()
+    {
+        var buyer = new Faker<Buyer>()
+            .RuleFor(b => b.Id, f => f.Random.Number(1, 20))
+            .RuleFor(b => b.Name, f => f.Name.FullName())
+            .Generate();
+        await _unitOfWork.BuyerRepository.Update(buyer);
+        await _unitOfWork.SaveChangesAsync();
+        var actual = await _unitOfWork.BuyerRepository.GetById(buyer.Id);
+        Assert.IsNotNull(actual);
+        Assert.That(actual.Name, Is.EqualTo(buyer.Name));
+    }
+
+    [Test]
+    public async Task DeleteBuyer()
+    {
+        var id = new Faker().Random.Number(1, 20);
+        var buyer = await _unitOfWork.BuyerRepository.GetById(id);
+        Assert.IsNotNull(buyer);
+        await _unitOfWork.BuyerRepository.Delete(buyer);
+        await _unitOfWork.SaveChangesAsync();
+        var actual = await _unitOfWork.BuyerRepository.GetById(id);
+        Assert.IsNull(actual);
+    }
+
+    [OneTimeTearDown]
+    public static async Task GlobalTearDown()
+    {
+        DatabaseTestHelper.ResetDatabase();
+    }
+}
--- Sales.Tests/Support/DatabaseTestHelper.cs
+++ Sales.Tests/Support/DatabaseTestHelper.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Bogus;
 using Microsoft.EntityFrameworkCore;
 using Sales.Contexts;
+using Sales.Entities.DbModels;
 using Sales.Entities.Mapping;
 using Sales.Repositories;
 using Sales.Repositories.Interfaces;
@@ -9,20 +11,117 @@ using Sales.Support;
 
 namespace Sales.Tests.Support;
 
-public static class TestHelper
+public static class DatabaseTestHelper
 {
     private static Context _context;
+    private static Faker _faker;
     
-    public static IUnitOfWork CreateUnitOfWork()
+    public static async Task<IUnitOfWork> CreateUnitOfWork()
     {
         _context = CreateTestContext();
         var mapper = CreateMapper();
+        _faker = new Faker("en");
         return new UnitOfWork(_context, mapper);
     }
+
+    public static async Task CreateFakeBuyers(int number = 20)
+    {
+        if (_faker == null)
+            throw new ArgumentNullException("Faker is not set");
+        var buyers = new Faker<BuyerDbModel>()
+            .RuleFor(b => b.Name, f => f.Name.FindName())
+            .Generate(number);
+        _context.Buyers.AddRange(buyers);
+        await _context.SaveChangesAsync();
+    }
+
+    public static async Task CreateFakeProducts(int number = 20)
+    {
+        if (_faker == null)
+            throw new ArgumentNullException("Faker is not set");
+        var products = new Faker<ProductDbModel>()
+            .RuleFor(p => p.Name, f => f.Commerce.ProductName())
+            .RuleFor(p => p.Price, f => f.Random.Decimal(1, 1000))
+            .Generate(number);
+        _context.Products.AddRange(products);
+        await _context.SaveChangesAsync();
+    }
+
+    public static async Task CreateFakeSalesPoints(int number = 10)
+    {
+        if (_faker == null)
+            throw new ArgumentNullException("Faker is not set");
+        var salesPoints = new Faker<SalesPointDbModel>()
+            .RuleFor(sp => sp.Name, f => f.Company.CompanyName())
+            .RuleFor(sp => sp.ProvidedProducts, f => new Faker<ProvidedProductDbModel>()
+                .RuleFor(pp => pp.ProductId, f => f.Random.Int(1, 20))
+                .RuleFor(pp => pp.ProductQuantity, f => f.Random.Int(1, 5))
+                .Generate(5)
+            )
+            .Generate(number);
+        _context.SalesPoints.AddRange(salesPoints);
+        await _context.SaveChangesAsync();
+    }
+
+    public static async Task CreateFakeSales(int number = 10, long buyerId = 1)
+    {
+        if (_faker == null)
+            throw new ArgumentNullException("Faker is not set");
+        if (!_context.Products.Any())
+            await CreateFakeProducts();
+        if (!_context.Buyers.Any())
+            await CreateFakeBuyers();
+        if (!_context.SalesPoints.Any())
+            await CreateFakeSalesPoints();
+
+        var sales = new Faker<SaleDbModel>()
+            .RuleFor(s => s.Date, f => DateOnly.FromDateTime(f.Date.Past()))
+            .RuleFor(s => s.Time, f => TimeOnly.FromDateTime(f.Date.Past()))
+            .RuleFor(s => s.SalesPointId, f => f.Random.Number(1, 10))
+            .RuleFor(s => s.BuyerId, f => f.Random.Number(1, 20))
+            .Generate(number);
+
+        var products = await _context.Products.ToListAsync();
+        foreach (var sale in sales)
+        {
+            var saleData = new List<SaleDataDbModel>();
+            for (int i = 0; i < 5; i++)
+            {
+                var productId = new Faker().Random.Number(1, 20);
+                var productQuantity = new Faker().Random.Number(1, 5);
+                var dbModel = new SaleDataDbModel()
+                {
+                    ProductId = productId,
+                    ProductQuantity = productQuantity,
+                    ProductIdAmount = products.First(p => p.Id == productId).Price * productQuantity
+                };
+                saleData.Add(dbModel);
+            }
+
+            sale.SaleData = saleData;
+        }
+        
+        sales.Add(
+            new ()
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now),
+                Time = TimeOnly.FromDateTime(DateTime.Now),
+                SalesPointId = 1,
+                BuyerId = buyerId,
+                SaleData = new List<SaleDataDbModel>(),
+            }
+        );
+        
+        _context.Sales.AddRange(sales);
+        await _context.SaveChangesAsync();
+    }
+    
     
     private static Context CreateTestContext()
     {
-        var connectionString = DotNetEnv.Env.GetString("DbConnectionString");
+        DotNetEnv.Env.Load(".env");
+        // var connectionString = Environment.GetEnvironmentVariable("DbConnectionString");
+        var connectionString = "Host=localhost;Port=5432;Database=TestSales;Username=phenirain;Password=ru13aaogh06(";
         if (string.IsNullOrEmpty(connectionString))
         {
             throw new InvalidOperationException("DbConnectionString is not set");
